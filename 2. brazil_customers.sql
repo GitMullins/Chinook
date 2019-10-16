21b83a766f53dba821efaca62cb9492cf1cead28
David Mullins <Mullins6323@gmail.com>
2019-10-15 20:28:28 -0500

    1-6 complete

---
+++ 2. brazil_customers.sql
@@ -0,0 +1,8 @@
+--brazil_customers.sql
+
+--Provide a query only showing the Customers from Brazil.
+
+
+select *
+from Customer
+where Country = 'Brazil'
---
+++ 4. sales_agents.sql
@@ -0,0 +1,7 @@
+--sales_agents.sql
+
+--Provide a query showing only the Employees who are Sales Agents
+
+Select *
+from Employee
+where Title = 'Sales Support Agent'
